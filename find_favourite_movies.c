a62fbfa82d995bd5f3267e6b4512fe8a7119f6d9
PTamm <tamm@ualberta.ca>
2018-11-10 00:38:52 -0700

    Update find_favourite_movies.c

---
+++ find_favourite_movies.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+
+int main(int argc, char *argv[]){
+    sqlite3 *db;
+    sqlite3_stmt *stmt;
+
+    int rc;
+    int col;
+    char *sql_stmt;
+
+
+    rc = sqlite3_open(argv[1], &db);
+
+    if (rc){
+        fprintf(stderr,"Can't open database: %s\n",sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    }
+
+    sql_stmt = "select title from movies;";
+
+    rc = sqlite3_prepare_v2(db,sql_stmt,-1,&stmt,0);
+
+    if (rc != SQLITE_OK){
+        fprintf(stderr,"Preparation failed: %s\n",sqlite_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    }
+
+    /*
+        Now we print the output ...
+    */
+
+    while((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+        for (col = 0; col < sqlite3_column_count(stmt); col++){
+            printf("%s",sqlite3_column_text);
+        }
+        printf("\n");
+    }
+
+    sqlite3_finalize(stmt);
+
+    return 0;
+}
